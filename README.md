8a68925d08e87f1e20b831c4a0f2f890aa5eb2b2
Masriadi <masriadi.doank@gmail.com>
2021-03-12 14:25:31 +0700

    Menyusun struktur direktori

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Belajar-Kotlin
 Referensi belajar kotlin
+
+sumber :
+1. Chanel youtube Programmer Zaman Now
+2. Chanel youtube Traversy Media
--- src/main/kotlin/dasar/Functions.kt
+++ src/main/kotlin/dasar/Functions.kt
@@ -10,8 +10,13 @@ fun bagi(a:Int, b:Int):Int {
     return a / b
 }
 
+fun kurang(a:Int, b:Int):Int {
+    return a - b
+}
+
 fun main() {
     println(tambah(3, 5))
     println(kali(3, 5))
     println(bagi(10, 5))
+    println(kurang(10, 5))
 }
