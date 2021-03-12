3435ad9566bcdbe7f72725aac57d36aa7abf8234
Masriadi <masriadi.doank@gmail.com>
2021-03-12 13:53:44 +0700

    Core commit

---
+++ src/main/kotlin/Functions.kt
@@ -0,0 +1,17 @@
+fun tambah(a:Int, b:Int):Int {
+    return a + b
+}
+
+fun kali(a:Int, b:Int):Int {
+    return a * b
+}
+
+fun bagi(a:Int, b:Int):Int {
+    return a / b
+}
+
+fun main() {
+    println(tambah(3, 5))
+    println(kali(3, 5))
+    println(bagi(10, 5))
+}
